2fce9c0d152f21c114c13a116950903c4522c559
Chad M <chadmusick@gmail.com>
2022-10-28 21:01:09 -0400

    cleared up some compile state errors

--- src/Components/Spells.js
+++ src/Components/Spells.js
@@ -3,44 +3,46 @@ import * as KnownSpells from "../Spells/KnownSpells";
 import { useEffect, useState } from "react";
 
 export const SpellListing = (props) => {
+   /* eslint-disable no-eval */
+  function handleCheck(event, item) {
+    let func = eval(`props.setLevel${item.level}`);
+    let tar = eval(`props.level${item.level}`);
 
-    function handleCheck(event, item) {
-        let func = eval(`props.setLevel${item.level}`);
-        let tar = eval(`props.level${item.level}`)
-        
-        if (event.target.checked === true) {
-            if(tar===0){
-                return event.preventDefault(),alert("No more spell slots");
-            }
-            func(tar-1);
+    if (event.target.checked === true) {
+      if (tar === 0) {
+        return (event.preventDefault(),
+        alert("No more spell slots"));
+      }
+      func(tar - 1);
     }
     if (event.target.checked === false) {
-        func(tar+1);
-}
-}
-
+      func(tar + 1);
+    }
+  }
 
   let lvlCheck = KnownSpells[props.selectedClass][props.level];
   let spellObject = SpellLists[props.selectedClass];
   const displayList = Object.values(spellObject)
     .filter((item) => lvlCheck[item.level] > 0)
-    .map((item, index) =>  <div key={index} className="form-check col-4">
-    <input
-      className="form-check-input"
-      type="checkbox"
-      value={item.spellName}
-      onChange={(event) => handleCheck(event, item)}
-    />
-    <label
-      style={{ fontWeight: "bold" }}
-      className="form-check-label"
-      htmlFor="flexCheckDefault"
-    >
-      {item.spellName}
-    </label></div>);
+    .map((item, index) => (
+      <div key={index} className="form-check col-4">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          value={item.spellName}
+          onChange={(event) => handleCheck(event, item)}
+        />
+        <label
+          style={{ fontWeight: "bold" }}
+          className="form-check-label"
+          htmlFor="flexCheckDefault"
+        >
+          {item.spellName}
+        </label>
+      </div>
+    ));
 
-  return displayList
-    
+  return displayList;
 };
 
 export const SpellsMain = (props) => {
@@ -56,6 +58,7 @@ export const SpellsMain = (props) => {
   const [level9, setLevel9] = useState(null);
 
   function vbl(key, value) {
+     /* eslint-disable no-eval */
     let input = eval(key);
     input(value);
   }
@@ -66,11 +69,11 @@ export const SpellsMain = (props) => {
     ).map(([key, value], index) => vbl(`setLevel${key}`, value));
   }, [props]);
 
-
   return (
     <div>
       <p>
-        Available Spells: {level0 && `Level 0: ${level0}`}{" "}
+        Available Spells: <br/>
+        {level0 && `Level 0: ${level0}`}{" "}
         {level1 && `Level 1: ${level1}`} {level2 && `Level 2: ${level2}`}
         {level3 && `Level 3: ${level3}`} {level4 && `Level 4: ${level4}`}{" "}
         {level5 && `Level 5: ${level5}`}
@@ -78,7 +81,30 @@ export const SpellsMain = (props) => {
         {level8 && `Level 8: ${level8}`} {level9 && `Level 9: ${level9}`}
       </p>
       <div className="d-flex flex-row flex-wrap">
-      <SpellListing level0={level0} setLevel0={setLevel0} level1={level1} setLevel1={setLevel1} selectedClass={props.selectedClass} level={props.level} />
+        <SpellListing
+          level0={level0}
+          setLevel0={setLevel0}
+          level1={level1}
+          setLevel1={setLevel1}
+          level2={level2}
+          setLevel2={setLevel2}
+          level3={level3}
+          setLevel3={setLevel3}
+          level4={level4}
+          setLevel4={setLevel4}
+          level5={level5}
+          setLevel5={setLevel5}
+          level6={level6}
+          setLevel6={setLevel6}
+          level7={level7}
+          setLevel7={setLevel7}
+          level8={level8}
+          setLevel8={setLevel8}
+          level9={level9}
+          setLevel9={setLevel9}
+          selectedClass={props.selectedClass}
+          level={props.level}
+        />
       </div>
     </div>
   );
