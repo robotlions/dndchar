b1353c4bc5acc3b0456309d2000d08901ec787d2
Chad M <chadmusick@gmail.com>
2022-11-03 18:26:54 -0400

    added through level 6 cleric spells

--- src/Spells/SpellListCleric.js
+++ src/Spells/SpellListCleric.js
@@ -143,4 +143,548 @@ export const ClericList = {
         spellName: "Summon Monster I",
         level: 1
     },
+    aid:{
+        spellName: "Aid",
+        level: 2
+    },
+    alignWeapon:{
+        spellName: "Align Weapon",
+        level: 2
+    },
+    augury:{
+        spellName: "Augury",
+        level: 2
+    },
+    bearsEndurance:{
+        spellName: "Bear's Endurance",
+        level: 2
+    },
+    bullsStrength:{
+        spellName: "Bull's Strength",
+        level: 2
+    },
+    calmEmotions:{
+        spellName: "Calm Emotions",
+        level: 2
+    },
+    consecrate:{
+        spellName: "Consecrate",
+        level: 2
+    },
+    cureModerateWounds:{
+        spellName: "Cure Moderate Wounds",
+        level: 2
+    },
+    darkness:{
+        spellName: "Darkness",
+        level: 2
+    },
+    deathKnell:{
+        spellName: "Death Knell",
+        level: 2
+    },
+    delayPoison:{
+        spellName: "Delay Poison",
+        level: 2
+    },
+    desecrate:{
+        spellName: "Desecrate",
+        level: 2
+    },
+    eaglesSplendor:{
+        spellName: "Eagle's Splendor",
+        level: 2
+    },
+    enthrall:{
+        spellName: "Enthrall",
+        level: 2
+    },
+    findTraps:{
+        spellName: "Find Traps",
+        level: 2
+    },
+    gentleRepose:{
+        spellName: "Gentle Repose",
+        level: 2
+    },
+    holdPerson:{
+        spellName: "Hold Person",
+        level: 2
+    },
+    inflictModerateWounds:{
+        spellName: "Inflict Moderate Wounds",
+        level: 2
+    },
+    makeWhole:{
+        spellName: "Make Whole",
+        level: 2
+    },
+    owlsWisdom:{
+        spellName: "Owl's Wisdom",
+        level: 2
+    },
+    removeParalysis:{
+        spellName: "Remove Paralysis",
+        level: 2
+    },
+    resistEnergy:{
+        spellName: "Resist Energy",
+        level: 2
+    },
+    restorationLesser:{
+        spellName: "Restoration, Lesser",
+        level: 2
+    },
+    shatter:{
+        spellName: "Shatter",
+        level: 2
+    },
+    shieldOther:{
+        spellName: "Shield Other",
+        level: 2
+    },
+    silence:{
+        spellName: "Silence",
+        level: 2
+    },
+    soundBurst:{
+        spellName: "Sound Burst",
+        level: 2
+    },
+    spiritualWeapon:{
+        spellName: "Spiritual Weapon",
+        level: 2
+    },
+    status:{
+        spellName: "Status",
+        level: 2
+    },
+    summonMonsterII:{
+        spellName: "Summon Monster II",
+        level: 2
+    },
+    undetectableAlignment:{
+        spellName: "Undetectable Alignment",
+        level: 2
+    },
+    zoneOfTruth:{
+        spellName: "Zone of Truth",
+        level: 2
+    },
+    animateDead:{
+        spellName: "Animate Dead",
+        level: 3
+    },
+    bestowCurse:{
+        spellName: "Bestow Curse",
+        level: 3
+    },
+    blindnessDeafness:{
+        spellName: "Blindness/ Deafness",
+        level: 3
+    },
+    contagion:{
+        spellName: "Contagion",
+        level: 3
+    },
+    continualFlame:{
+        spellName: "Continual Flame",
+        level: 3
+    },
+    createFoodAndWater:{
+        spellName: "Create Food and Water",
+        level: 3
+    },
+    cureSeriousWounds:{
+        spellName: "Cure Serious Wounds",
+        level: 3
+    },
+    daylight:{
+        spellName: "Daylight",
+        level: 3
+    },
+    deeperDarkness:{
+        spellName: "Deeper Darkness",
+        level: 3
+    },
+    dispelMagic:{
+        spellName: "Dispel Magic",
+        level: 3
+    },
+    glyphOfWarding:{
+        spellName: "Glyph of Warding",
+        level: 3
+    },
+    helpingHand:{
+        spellName: "Helping Hand",
+        level: 3
+    },
+    inflictSeriousWounds:{
+        spellName: "Inflict Serious Wounds",
+        level: 3
+    },
+    invisibilityPurge:{
+        spellName: "Invisibility Purge",
+        level: 3
+    },
+    locateObject:{
+        spellName: "Locate Object",
+        level: 3
+    },
+    magicCircleAgainstChaosEvilGoodLaw:{
+        spellName: "Magic Circle Against Chaos/ Evil/ Good/ Law",
+        level: 3
+    },
+    magicVestment:{
+        spellName: "Magic Vestment",
+        level: 3
+    },
+    meldIntoStone:{
+        spellName: "Melt Into Stone",
+        level: 3
+    },
+    obscureObject:{
+        spellName: "Obscure Object",
+        level: 3
+    },
+    prayer:{
+        spellName: "Prayer",
+        level: 3
+    },
+    protectionFromEnergy:{
+        spellName: "Protection From Energy",
+        level: 3
+    },
+    removeBlindnessDeafness:{
+        spellName: "Remove Blindness/ Deafness",
+        level: 3
+    },
+    removeCurse:{
+        spellName: "Remove Curse",
+        level: 3
+    },
+    removeDisease:{
+        spellName: "Remove Disease",
+        level: 3
+    },
+    searingLight:{
+        spellName: "Searing Light",
+        level: 3
+    },
+    speakWithDead:{
+        spellName: "Speak With Dead",
+        level: 3
+    },
+    stoneShape:{
+        spellName: "Stone Shape",
+        level: 3
+    },
+    summonMonsterIII:{
+        spellName: "Summon Monster III",
+        level: 3
+    },
+    waterBreathing:{
+        spellName: "Water Breathing",
+        level: 3
+    },
+    waterWalk:{
+        spellName: "Water Walk",
+        level: 3
+    },
+    windWall:{
+        spellName: "Wind Wall",
+        level: 3
+    },
+    airWalk:{
+        spellName: "Air Walk",
+        level: 4
+    },
+    controlWater:{
+        spellName: "Control Water",
+        level: 4
+    },
+    cureCriticalWounds:{
+        spellName: "Cure Critical Wounds",
+        level: 4
+    },
+    deathWard:{
+        spellName: "Death Ward",
+        level: 4
+    },
+    dimensionalAnchor:{
+        spellName: "Dimensional Anchor",
+        level: 4
+    },
+    discernLies:{
+        spellName: "Discern Lies",
+        level: 4
+    },
+    dismissal:{
+        spellName: "Dismissal",
+        level: 4
+    },
+    divination:{
+        spellName: "Divination",
+        level: 4
+    },
+    divinePower:{
+        spellName: "Divine Power",
+        level: 4
+    },
+    freedomOfMovement:{
+        spellName: "Freedom Of Movement",
+        level: 4
+    },
+    giantVermin:{
+        spellName: "Giant Vermin",
+        level: 4
+    },
+    imbueWithSpellAbility:{
+        spellName: "Imbue With Spell Ability",
+        level: 4
+    },
+    inflictCriticalWounds:{
+        spellName: "Inflict Critical Wounds",
+        level: 4
+    },
+    magicWeaponGreater:{
+        spellName: "Magic Weapon, Greater",
+        level: 4
+    },
+    neutralizePoison:{
+        spellName: "Neutralize Poison",
+        level: 4
+    },
+    planarAllyLesser:{
+        spellName: "Planar Ally, Lesser",
+        level: 4
+    },
+    poison:{
+        spellName: "Poison",
+        level: 4
+    },
+    repelVermin:{
+        spellName: "Repel Vermin",
+        level: 4
+    },
+    restoration:{
+        spellName: "Restoration",
+        level: 4
+    },
+    sending:{
+        spellName: "Sending",
+        level: 4
+    },
+    spellImmunity:{
+        spellName: "Spell Immunity",
+        level: 4
+    },
+    summonMonsterIV:{
+        spellName: "Summon Monster IV",
+        level: 4
+    },
+    tongues:{
+        spellName: "Tongues",
+        level: 4
+    },
+    atonement:{
+        spellName: "Atonement",
+        level: 5
+    },
+    breakEnchantment:{
+        spellName: "Break Enchantment",
+        level: 5
+    },
+    commandGreater:{
+        spellName: "Command, Greater",
+        level: 5
+    },
+    commune:{
+        spellName: "Commune",
+        level: 5
+    },
+    cureLightWoundsMass:{
+        spellName: "Cure Light Wounds, Mass",
+        level: 5
+    },
+    dispelChaosEvilGoodLaw:{
+        spellName: "Dispel Chaos/ Evil/ Good/ Law",
+        level: 5
+    },
+    disruptingWeapon:{
+        spellName: "Disrupting Weapon",
+        level: 5
+    },
+    flameStrike:{
+        spellName: "Flame Strike",
+        level: 5
+    },
+    hallow:{
+        spellName: "Hallow",
+        level: 5
+    },
+    inflictLightWoundsMass:{
+        spellName: "Inflict Light Wounds, Mass",
+        level: 5
+    },
+    insectPlague:{
+        spellName: "Insect Plague",
+        level: 5
+    },
+    markOfJustice:{
+        spellName: "Mark of Justice",
+        level: 5
+    },
+    planeShift:{
+        spellName: "Plane Shift",
+        level: 5
+    },
+    raiseDead:{
+        spellName: "Raise Dead",
+        level: 5
+    },
+    righteousMight:{
+        spellName: "Righteous Might",
+        level: 5
+    },
+    scrying:{
+        spellName: "Scrying",
+        level: 5
+    },
+    slayLiving:{
+        spellName: "Slay Living",
+        level: 5
+    },
+    spellResistance:{
+        spellName: "Spell Resistance",
+        level: 5
+    },
+    summonMonsterV:{
+        spellName: "Summon Monster V",
+        level: 5
+    },
+    symbolOfPain:{
+        spellName: "Symbol of Pain",
+        level: 5
+    },
+    symbolOfSleep:{
+        spellName: "Symbol of Sleep",
+        level: 5
+    },
+    trueSeeing:{
+        spellName: "True Seeing",
+        level: 5
+    },
+    unhallow:{
+        spellName: "Unhallow",
+        level: 5
+    },
+    wallOfStone:{
+        spellName: "Wall of Stone",
+        level: 5
+    },
+    animateObjects:{
+        spellName: "Animate Objects",
+        level: 6
+    },
+    antilifeShell:{
+        spellName: "Antilife Shell",
+        level: 6
+    },
+    banishment:{
+        spellName: "Banishment",
+        level: 6
+    },
+    bearsEnduranceMass:{
+        spellName: "Bear's Endurance, Mass",
+        level: 6
+    },
+    bladeBarrier:{
+        spellName: "Blade Barrier",
+        level: 6
+    },
+    bullsStrengthMass:{
+        spellName: "Bull's Strength, Mass",
+        level: 6
+    },
+    createUndead:{
+        spellName: "Create Undead",
+        level: 6
+    },
+    cureModerateWoundsMass:{
+        spellName: "Cure Moderate Wounds, Mass",
+        level: 6
+    },
+    dispelMagicGreater:{
+        spellName: "Dispel Magic, Greater",
+        level: 6
+    },
+    eaglesSplendorMass:{
+        spellName: "Eagle's Splendor, Mass",
+        level: 6
+    },
+    findThePath:{
+        spellName: "Find the Path",
+        level: 6
+    },
+    forbiddance:{
+        spellName: "Forbiddance",
+        level: 6
+    },
+    geasQuest:{
+        spellName: "Geas/ Quest",
+        level: 6
+    },
+    glyphOfWardingGreater:{
+        spellName: "Glyph of Warding, Greater",
+        level: 6
+    },
+    harm:{
+        spellName: "Harm",
+        level: 6
+    },
+    heal:{
+        spellName: "Heal",
+        level: 6
+    },
+    heroesFeast:{
+        spellName: "Heroes' Feast",
+        level: 6
+    },
+    inflictModerateWoundsMass:{
+        spellName: "Inflict Moderate Wounds, Mass",
+        level: 6
+    },
+    owlsWisdomMass:{
+        spellName: "Owl's Wisdom, Mass",
+        level: 6
+    },
+    planarAlly:{
+        spellName: "Planar Ally",
+        level: 6
+    },
+    summonMonsterVI:{
+        spellName: "Summon Monster VI",
+        level: 6
+    },
+    symbolOfFear:{
+        spellName: "Symbol of Fear",
+        level: 6
+    },
+    symbolOfPersuasion:{
+        spellName: "Symbol of Persuasion",
+        level: 6
+    },
+    undeathToDeath:{
+        spellName: "Undeath to Death",
+        level: 6
+    },
+    windWalk:{
+        spellName: "Wind Walk",
+        level: 6
+    },
+    wordOfRecall:{
+        spellName: "Word of Recall",
+        level: 6
+    },
 };
