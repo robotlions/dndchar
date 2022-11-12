1428be1d9db8c4f5ca1462a79fba1c8a4516f980
Chad M <chadmusick@gmail.com>
2022-11-12 18:22:59 -0500

    adds useEffect to update base attack

--- src/Components/BaseAttack.js
+++ src/Components/BaseAttack.js
@@ -1,23 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as BaseAttackTables from '../Classes/BaseAttackTables';
 
 export const BaseAttack = (props) => {
+
+    const [baseAttackDisplay, setBaseAttackDisplay] = useState(0);
 
     function calculateModifier(abil){
         return -5 + Math.floor(1*(abil/2))
       }
 
+// useEffect(()=>{
+//     parseInt(props.setBaseAttack(BaseAttackTables[props.selectedClass][props.level]))
+// },[props.selectedClass, props.str, props.level])
+
 useEffect(()=>{
-    props.setBaseAttack(BaseAttackTables[props.selectedClass][props.level]+attackMod)
-},[props.selectedClass, props.str, props.level])
+    let attackMod = calculateModifier(props.str);
+    let baseAttackBase = parseInt(BaseAttackTables[props.selectedClass][props.level])+attackMod;
+    setBaseAttackDisplay(baseAttackBase);
+    props.setBaseAttack(baseAttackBase);
+},[props.level, props.selectedClass, props.str]);
 
-const attackMod = calculateModifier(props.str)
+// const attackMod = calculateModifier(props.str)
 
-const displayBaseAttacks = BaseAttackTables[props.selectedClass][props.level].map((item, index)=><span key={index}>+{item+attackMod}   </span>)
+// const displayBaseAttacks = BaseAttackTables[props.selectedClass][props.level].map((item, index)=><span key={index}>+{parseInt(item+attackMod)}   </span>)
 
     return(<div style={{textAlign: "center"}}>
 <h6 style={{fontWeight: "bold"}}>Base Attack Bonus</h6>
-        {displayBaseAttacks}
+        {baseAttackDisplay}
         </div>
     )
-}
+};
