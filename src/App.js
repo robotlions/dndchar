ae91c53e47371e2162b1afa2b9ee66e2990492ce
Chad M <chadmusick@gmail.com>
2024-07-20 18:39:32 -0400

    adds restart button

--- src/App.js
+++ src/App.js
@@ -139,42 +139,58 @@ function App() {
           setFontThemeFantasy={setFontThemeFantasy}
           setMunchkinMode={setMunchkinMode}
         />
-        <div className={
-          fontThemeFantasy === false
-            ? "container font-standard"
-            : "container font-fantasy"
-        } style={{ textAlign: "center" }}>
-         <h5 style={{paddingTop:"20px",marginBottom:"20px"}}>Would you like to create your Dungeons and Dragons 3.5 character in <strong>lawful mode</strong> or <strong>chaotic mode</strong>?
-         </h5>
-        
+        <div
+          className={
+            fontThemeFantasy === false
+              ? "container font-standard"
+              : "container font-fantasy"
+          }
+          style={{ textAlign: "center" }}
+        >
+          <h5 style={{ paddingTop: "20px", marginBottom: "20px" }}>
+            Would you like to create your Dungeons and Dragons 3.5 character in{" "}
+            <strong>lawful mode</strong> or <strong>chaotic mode</strong>?
+          </h5>
+
           <div className="row">
-            <div className="col-lg-6" style={{marginBottom: "10px"}}>
-         <p><h4 style={{ fontFamily: fontCheck }}>Lawful</h4>Roll random ability scores and create a first-level character in accordance with the 2003 <em>Player's Handbook</em>.</p>
-<div className="row">
-  <div className="col">
-              <Button
-                variant="secondary"
-                onClick={() => setModeChosen(true)}
-              >Lawful</Button>
+            <div className="col-lg-6" style={{ marginBottom: "10px" }}>
+              <p>
+                <h4 style={{ fontFamily: fontCheck }}>Lawful</h4>Roll random
+                ability scores and create a first-level character in accordance
+                with the 2003 <em>Player's Handbook</em>.
+              </p>
+              <div className="row">
+                <div className="col">
+                  <Button
+                    variant="secondary"
+                    onClick={() => setModeChosen(true)}
+                  >
+                    Lawful
+                  </Button>
+                </div>
               </div>
+            </div>
+
+            <div className="col-lg-6">
+              <p>
+                <h4 style={{ fontFamily: fontCheck }}>Chaotic</h4> Manually set
+                level and ability scores. Start with a million silver. We
+                disavow all characters made this way.
+              </p>
+              <div className="row">
+                <div className="col">
+                  <Button
+                    variant="secondary"
+                    onClick={() => {
+                      setMunchkinMode(true);
+                      setModeChosen(true);
+                    }}
+                  >
+                    Chaotic
+                  </Button>
+                </div>
               </div>
-              </div>
-             
-              <div className="col-lg-6">
-         <p><h4 style={{ fontFamily: fontCheck }}>Chaotic</h4> Manually set level and ability scores. Start with a million silver. We disavow all characters made this way.</p>
-         <div className="row">
-         <div className="col">
-              <Button
-                variant="secondary"
-                onClick={() => {
-                  setMunchkinMode(true);
-                  setModeChosen(true);
-                }}
-              >
-                Chaotic
-              </Button>
-              </div></div>
-              </div>
+            </div>
           </div>
         </div>
       </>
@@ -196,7 +212,10 @@ function App() {
           setFontThemeFantasy={setFontThemeFantasy}
           setMunchkinMode={setMunchkinMode}
         />
-
+        <div style={{textAlign:"center"}}>
+          <br/>
+<Button variant="secondary" onClick={()=>window.location.reload()}>Start Over</Button>
+</div>
         <br />
         {/* <Accordion defaultActiveKey={['0']} alwaysOpen> */}
         <Accordion alwaysOpen>
@@ -530,6 +549,9 @@ function App() {
               {/* <Button name="printCharacterButton" variant="secondary" onClick={(e)=>{console.log(e)}}> */}
               View and Print Character
             </Button>
+            <br/><br/>
+<Button variant="secondary" onClick={()=>window.location.reload()}>Start Over</Button>
+
           </div>
         </div>
       </div>
--- src/Components/AbilityScores.js
+++ src/Components/AbilityScores.js
@@ -13,21 +13,19 @@ function roll() {
     .reduce((a, b) => a + b, 0);
 }
 
-
-
-function calculateModifier(abil){
-  return -5 + Math.floor(1*(abil/2))
+function calculateModifier(abil) {
+  return -5 + Math.floor(1 * (abil / 2));
 }
 
 export const NewScores = (props) => {
   useEffect(() => {
     if (newRoll === true) {
-      let conRoll=roll();
-      let dexRoll=roll();
-      let wisRoll=roll();
-      let intRoll=roll();
-      let chrRoll=roll();
-      let strRoll=roll();
+      let conRoll = roll();
+      let dexRoll = roll();
+      let wisRoll = roll();
+      let intRoll = roll();
+      let chrRoll = roll();
+      let strRoll = roll();
       setStr(strRoll);
       setInt(intRoll);
       setWis(wisRoll);
@@ -52,118 +50,205 @@ export const NewScores = (props) => {
   const [dex, setDex] = useState(10);
   const [chr, setChr] = useState(10);
 
-
-  const rerollButton = <Button variant="secondary" onClick={() => rollButton()}>Roll Abilities</Button>;
+  const rerollButton = (
+    <Button variant="secondary" onClick={() => rollButton()}>
+      Roll Abilities
+    </Button>
+  );
   const racialBonus = RaceBonuses[props.selectedRace];
 
-  function rollButton(){
-    if(props.selectedRace==="select"){
-      return alert("Please select a race first")
-    }
-    else{
+  function rollButton() {
+    if (props.selectedRace === "select") {
+      return alert("Please select a race first");
+    } else {
       setNewRoll(true);
       props.setRolled(true);
     }
   }
 
-  
-
-
   return (
     <div>
       <table>
         <thead>
-        <tr>
-          <th>Ability</th>
-          <th>Roll</th>
-          <th>Racial Bonus</th>
-          <th>Total Score</th>
-          <th>Modifier</th>
-        </tr>
+          <tr>
+            <th>Ability</th>
+            <th>Roll</th>
+            <th>Racial Bonus</th>
+            <th>Total Score</th>
+            <th>Modifier</th>
+          </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>STR</td>
-          {props.munchkinMode===true ? <td><input className="attInput" onChange={(e)=>{setStr(parseInt(e.target.value));props.setStr(parseInt(e.target.value))}} placeholder={str || 0}></input></td> :
-          <td>{str}</td>}
-          <td>
-            {racialBonus.bonusStr <= 0
-              ? racialBonus.bonusStr
-              : racialBonus.bonusStr}
-          </td>
-          <td className="totalScore">{str + racialBonus.bonusStr}</td>
+          <tr>
+            <td>STR</td>
+            {props.munchkinMode === true ? (
+              <td>
+                <input
+                  className="attInput"
+                  onChange={(e) => {
+                    setStr(parseInt(e.target.value));
+                    props.setStr(parseInt(e.target.value));
+                  }}
+                  placeholder={str || 0}
+                  onFocus={(e) => (e.target.placeholder = "")}
+                  onBlur={(e) => (e.target.placeholder = str)}
+                ></input>
+              </td>
+            ) : (
+              <td>{str}</td>
+            )}
+            <td>
+              {racialBonus.bonusStr <= 0
+                ? racialBonus.bonusStr
+                : racialBonus.bonusStr}
+            </td>
+            <td className="totalScore">{str + racialBonus.bonusStr}</td>
 
-          <td>{calculateModifier(str+racialBonus.bonusStr)}</td>
-        </tr>
-        <tr>
-          <td>DEX</td>
-          {props.munchkinMode===true ? <td><input className="attInput" onChange={(e)=>{setDex(parseInt(e.target.value));props.setDex(parseInt(e.target.value))}} placeholder={dex}></input></td> :
-<td>{dex}</td>}
-          <td>
-            {racialBonus.bonusDex <= 0
-              ? racialBonus.bonusDex
-              : racialBonus.bonusDex}
-          </td>
-          <td className="totalScore">{dex + racialBonus.bonusDex}</td>
+            <td>{calculateModifier(str + racialBonus.bonusStr)}</td>
+          </tr>
+          <tr>
+            <td>DEX</td>
+            {props.munchkinMode === true ? (
+              <td>
+                <input
+                  className="attInput"
+                  onChange={(e) => {
+                    setDex(parseInt(e.target.value));
+                    props.setDex(parseInt(e.target.value));
+                  }}
+                  placeholder={dex || 0}
+                  onFocus={(e) => (e.target.placeholder = "")}
+                  onBlur={(e) => (e.target.placeholder = dex)}
+                ></input>
+              </td>
+            ) : (
+              <td>{dex}</td>
+            )}
+            <td>
+              {racialBonus.bonusDex <= 0
+                ? racialBonus.bonusDex
+                : racialBonus.bonusDex}
+            </td>
+            <td className="totalScore">{dex + racialBonus.bonusDex}</td>
 
-          <td>{calculateModifier(dex+racialBonus.bonusDex)}</td>
-        </tr>
-        <tr>
-          <td>CON</td>
-          {props.munchkinMode===true ? <td><input className="attInput" onChange={(e)=>{setCon(parseInt(e.target.value));props.setCon(parseInt(e.target.value))}} placeholder={con || 0}></input></td> :
-          <td>{con}</td>}
-          <td>
-            {racialBonus.bonusCon <= 0
-              ? racialBonus.bonusCon
-              : racialBonus.bonusCon}
-          </td>
-          <td className="totalScore">{con + racialBonus.bonusCon}</td>
+            <td>{calculateModifier(dex + racialBonus.bonusDex)}</td>
+          </tr>
+          <tr>
+            <td>CON</td>
+            {props.munchkinMode === true ? (
+              <td>
+                <input
+                  className="attInput"
+                  onChange={(e) => {
+                    setCon(parseInt(e.target.value));
+                    props.setCon(parseInt(e.target.value));
+                  }}
+                  placeholder={con || 0}
+                  onFocus={(e) => (e.target.placeholder = "")}
+                  onBlur={(e) => (e.target.placeholder = con)}
+                ></input>
+              </td>
+            ) : (
+              <td>{con}</td>
+            )}
+            <td>
+              {racialBonus.bonusCon <= 0
+                ? racialBonus.bonusCon
+                : racialBonus.bonusCon}
+            </td>
+            <td className="totalScore">{con + racialBonus.bonusCon}</td>
 
-          <td>{calculateModifier(con+racialBonus.bonusCon)}</td>
-        </tr>
-        <tr>
-          <td>INT</td>
-          {props.munchkinMode===true ? <td><input className="attInput" onChange={(e)=>{setInt(parseInt(e.target.value));props.setInt(parseInt(e.target.value))}} placeholder={int || 0}></input></td> :
-          <td>{int}</td>}
-          <td>
-            {racialBonus.bonusInt <= 0
-              ? racialBonus.bonusInt
-              : racialBonus.bonusInt}
-          </td>
-          <td className="totalScore">{int + racialBonus.bonusInt}</td>
+            <td>{calculateModifier(con + racialBonus.bonusCon)}</td>
+          </tr>
+          <tr>
+            <td>INT</td>
+            {props.munchkinMode === true ? (
+              <td>
+                <input
+                  className="attInput"
+                  onChange={(e) => {
+                    setInt(parseInt(e.target.value));
+                    props.setInt(parseInt(e.target.value));
+                  }}
+                  placeholder={int || 0}
+                  onFocus={(e) => (e.target.placeholder = "")}
+                  onBlur={(e) => (e.target.placeholder = int)}
+                ></input>
+              </td>
+            ) : (
+              <td>{int}</td>
+            )}
+            <td>
+              {racialBonus.bonusInt <= 0
+                ? racialBonus.bonusInt
+                : racialBonus.bonusInt}
+            </td>
+            <td className="totalScore">{int + racialBonus.bonusInt}</td>
 
-          <td>{calculateModifier(int+racialBonus.bonusInt)}</td>
-        </tr>
-        <tr>
-          <td>WIS</td>
-          {props.munchkinMode===true ? <td><input className="attInput" onChange={(e)=>{setWis(parseInt(e.target.value));props.setWis(parseInt(e.target.value))}} placeholder={wis || 0}></input></td> :
-          <td>{wis}</td>}
-          <td>
-            {racialBonus.bonusWis <= 0
-              ? racialBonus.bonusWis
-              : racialBonus.bonusWis}
-          </td>
-          <td className="totalScore">{wis + racialBonus.bonusWis}</td>
+            <td>{calculateModifier(int + racialBonus.bonusInt)}</td>
+          </tr>
+          <tr>
+            <td>WIS</td>
+            {props.munchkinMode === true ? (
+              <td>
+                <input
+                  className="attInput"
+                  onChange={(e) => {
+                    setWis(parseInt(e.target.value));
+                    props.setWis(parseInt(e.target.value));
+                  }}
+                  placeholder={wis || 0}
+                  onFocus={(e) => (e.target.placeholder = "")}
+                  onBlur={(e) => (e.target.placeholder = wis)}
+                ></input>
+              </td>
+            ) : (
+              <td>{wis}</td>
+            )}
+            <td>
+              {racialBonus.bonusWis <= 0
+                ? racialBonus.bonusWis
+                : racialBonus.bonusWis}
+            </td>
+            <td className="totalScore">{wis + racialBonus.bonusWis}</td>
 
-          <td>{calculateModifier(wis+racialBonus.bonusWis)}</td>
-        </tr>
-        
-        <tr>
-          <td>CHR</td>
-          {props.munchkinMode===true ? <td><input className="attInput" onChange={(e)=>{setChr(parseInt(e.target.value));props.setChr(parseInt(e.target.value))}} placeholder={chr || 0}></input></td> :
-          <td>{chr}</td>}
-          <td>
-            {racialBonus.bonusChr <= 0
-              ? racialBonus.bonusChr
-              : racialBonus.bonusChr}
-          </td>
-          <td className="totalScore">{chr + racialBonus.bonusChr}</td>
+            <td>{calculateModifier(wis + racialBonus.bonusWis)}</td>
+          </tr>
 
-          <td>{calculateModifier(chr+racialBonus.bonusChr)}</td>
-        </tr>
+          <tr>
+            <td>CHR</td>
+            {props.munchkinMode === true ? (
+              <td>
+                <input
+                  className="attInput"
+                  onChange={(e) => {
+                    setChr(parseInt(e.target.value));
+                    props.setChr(parseInt(e.target.value));
+                  }}
+                  placeholder={chr || 0}
+                  onFocus={(e) => (e.target.placeholder = "")}
+                  onBlur={(e) => (e.target.placeholder = ChannelSplitterNode)}
+                ></input>
+              </td>
+            ) : (
+              <td>{chr}</td>
+            )}
+            <td>
+              {racialBonus.bonusChr <= 0
+                ? racialBonus.bonusChr
+                : racialBonus.bonusChr}
+            </td>
+            <td className="totalScore">{chr + racialBonus.bonusChr}</td>
+
+            <td>{calculateModifier(chr + racialBonus.bonusChr)}</td>
+          </tr>
         </tbody>
       </table>
-      <p style={{textAlign: "center", marginTop: 10}}>{rerollButton}</p>
+      <p style={{ textAlign: "center", marginTop: 10 }}>
+        {props.munchkinMode === true
+          ? "Chaos Mode. Set abilities manually."
+          : rerollButton}
+      </p>
     </div>
   );
 };
