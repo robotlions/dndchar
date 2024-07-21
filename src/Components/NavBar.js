cd0791fce1b5755e82bfc30244bc4ee3ce2455b0
Chad M <chadmusick@gmail.com>
2024-07-20 22:39:40 -0400

    cleans up banner and nav position

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,6 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import {useState } from "react";
-// import dndModLogo from "../images/dndModLogo.png";
 import dnd2000Logo from "../images/dnd2000Logo.png";
 
 export const TopNav = (props) => {
@@ -22,14 +21,6 @@ export const TopNav = (props) => {
     }
   }
 
-  function handleMunchkinSwitch(event) {
-    if (event.target.checked === true) {
-      props.setMunchkinMode(true);
-      alert("Munchkin Mode allows you to manually adjust the character's level and ability scores and it sets the silver to a million. Warning: this generator works best for level one characters. At higher levels, things get unreliable.");
-    } else {
-      props.setMunchkinMode(false);
-    }
-  }
 
   const fontCheck = props.fontThemeFantasy===true ? "eagle-lake" : "gotham-black"
   const currentDate = new Date();
@@ -37,45 +28,21 @@ export const TopNav = (props) => {
 
   return (
     <>
+    
+           
+          
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand
-            style={{ textAlign: "center", whiteSpace: "normal" }}
-           
-          >
-            <img alt="modern dnd logo" src={dnd2000Logo} style={{maxWidth:"30%"}}></img>
-            <h3 style={{ fontFamily: fontCheck }}>
-              3.5 Edition<br/>Character Creator
-            </h3>
-          </Navbar.Brand>
-          <Navbar.Toggle className="ms-auto" aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse   style={{textAlign: "center"}} id="basic-navbar-nav">
-            <Nav className="me-auto">
+          
+          <Navbar.Toggle className="ml-auto" aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
               <Nav.Link onClick={() => handleShow()}>About</Nav.Link>
               <Nav.Link href="https://robotlions.com" target="_blank">
                 robotlions.com
               </Nav.Link>
-
-              <NavDropdown title="Options" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">
-                  {/* <div className="form-check form-switch">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      role="switch"
-                      onChange={(event) => handleMunchkinSwitch(event)}
-                      id="flexSwitchCheckMunchkin"
-                    />
-                    <label
-                      className="form-check-label"
-                      htmlFor="flexSwitchCheckMunchkin"
-                    >
-                      Munchkin Mode
-                    </label>
-                  </div> */}
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  <div className="form-check form-switch">
+              <Nav.Item>
+              <div className="form-check form-switch" style={{paddingTop:"8px"}}>
                     <input
                       className="form-check-input"
                       type="checkbox"
@@ -84,23 +51,27 @@ export const TopNav = (props) => {
                       id="flexSwitchCheckFont"
                     />
                     <label
+                    style={{color: "rgb(102, 108, 109)"}}
                       className="form-check-label"
                       htmlFor="flexSwitchCheckFont"
                     >
                       Rad fantasy font
                     </label>
                   </div>
-                </NavDropdown.Item>
-                {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item> */}
-              </NavDropdown>
+              </Nav.Item>
+
+
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <img alt="modern dnd logo" src={dnd2000Logo} style={{display: "block",
+  marginLeft: "auto", marginRight: "auto",maxWidth:"50%"}}></img>
+  <div style={{textAlign:"center"}}>
+            <h3 style={{ fontFamily: fontCheck }}>
+              3.5 Edition<br/>Character Creator
+            </h3>
+            </div>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton></Modal.Header>
